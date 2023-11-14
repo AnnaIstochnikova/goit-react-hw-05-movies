0f2d68ecf2c5208dd6d36890e32cf92cb39698b9
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-11-14 20:12:15 +0100

    Styled component

---
+++ src/components/search-form/SearchForm.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  border-radius: 15px;
+  border-color: orange;
+  height: 20px;
+  padding-left: 5px;
+`;
+
+export const SearchButton = styled.button`
+  margin-left: 15px;
+  border-radius: 15px;
+  height: 27px;
+  border-color: orange;
+  font-weight: 600;
+`;
